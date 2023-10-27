aeda5acba27af53effcfbee796e6afe363e05b0e
RaMandeep2002 <ramandeepsigh1511@gmail.com>
2023-10-27 15:33:48 +0530

    some changes in file

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -6,27 +6,37 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./masterColab.sol";
 import "./MasterChef.sol";
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-contract Factory {
+contract Factory is Initializable {
+    uint256 a;
     masterColab[] public data;
     MasterChef public master;
     address public masterContract;
 
-    constructor(address _masterContract, address _master) {
+    // constructor(address _masterContract, address _master) {
+    //     masterContract = _masterContract;
+    //     master = MasterChef(_master);
+    // }
+
+    function initialize(
+        address _masterContract,
+        address _master
+    ) public initializer {
         masterContract = _masterContract;
         master = MasterChef(_master);
     }
- 
+
     function createChild() public {
         // console.log('check CreateChild');
         masterColab child = masterColab(Clones.clone(masterContract));
-        child.initailize(address(this),address(master)); 
-        data.push(child); 
-        // console.log('address(child)', address(child)); 
-    }  
+        child.initailize(address(this), address(master));
+        data.push(child);
+        // console.log('address(child)', address(child));
+    }
 
-    function getData() external view returns (masterColab ) {
-        uint x = data.length -1;
-        return data[x]; 
+    function getData() external view returns (masterColab) {
+        uint x = data.length - 1;
+        return data[x];
     }
 }
